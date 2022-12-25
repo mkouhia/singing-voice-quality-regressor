e536c0e94392f6fb070270586841e0a328aeaae1
Mikko Kouhia <mikko.kouhia@iki.fi>
2022-12-25 09:04:33 +0200

    Extract audio sources from dataframe

---
+++ singing_classifier/etl.py
@@ -0,0 +1,61 @@
+"""Download data files for the SVQTD dataset"""
+
+from dataclasses import dataclass
+from os import PathLike
+from typing import Callable
+
+import pandas as pd
+
+
+@dataclass(frozen=True, eq=True)
+class AudioSegment:
+    """Time segment in audio source."""
+
+    num: int
+    start_time: float
+    end_time: float
+
+
+@dataclass(frozen=True, eq=True)
+class AudioSource:
+    """Audio source, with segment information."""
+
+    name: str
+    url: str
+    segments: frozenset[AudioSegment]
+
+
+def extract_audio_sources(
+    source_csv: PathLike,
+    url_creator: Callable,
+    ignore_names: list[str] = None,
+) -> list[AudioSource]:
+    data = pd.read_csv(source_csv)[["name", "num", "time_start", "time_end"]]
+    ignore_names = ignore_names or []
+
+    grouped = data.groupby("name")
+    return [
+        _grouped_df_to_audiosource(
+            name=group_name,
+            data=grouped.get_group(group_name),
+            url_creator=url_creator,
+        )
+        for group_name in grouped.groups
+        if group_name not in ignore_names
+    ]
+
+
+def _grouped_df_to_audiosource(
+    name: str, data: pd.DataFrame, url_creator: Callable
+) -> AudioSource:
+    segments = (
+        data.sort_values(by="num", ascending=True)
+        .apply(_row_to_segment, axis=1)
+        .tolist()
+    )
+    return AudioSource(name=name, url=url_creator(name), segments=frozenset(segments))
+
+
+def _row_to_segment(row):
+    return AudioSegment(row["num"], row["time_start"], row["time_end"])
+
---
+++ tests/test_etl.py
@@ -0,0 +1,54 @@
+"""Test data extraction, transformation and loading."""
+
+from pathlib import Path
+
+from singing_classifier.etl import (
+    AudioSegment,
+    AudioSource,
+    extract_audio_sources,
+)
+
+
+def test_extract_audio_sources(tmp_path: Path):
+    """Convert CSV file into list of audio sources."""
+    content = """name,num,time_start,time_end
+test,0,0.0,0.0
+0HVjTPqe5I0,0,8.893,17.323
+0HVjTPqe5I0,1,18.053,28.832
+0HVjTPqe5I0,2,39.12,53.974
+0QzJ86rMHzQ,0,7.464,25.834"""
+    csv_loc = tmp_path / "test.csv"
+    csv_loc.write_text(content)
+    
+    def url_creator(tag):
+        return "abc" + tag
+
+    expected = {
+        AudioSource(
+            "0HVjTPqe5I0",
+            url_creator("0HVjTPqe5I0"),
+            frozenset(
+                [
+                    AudioSegment(0, 8.893, 17.323),
+                    AudioSegment(1, 18.053, 28.832),
+                    AudioSegment(2, 39.12, 53.974),
+                ]
+            ),
+        ),
+        AudioSource(
+            "0QzJ86rMHzQ",
+            url_creator("0QzJ86rMHzQ"),
+            frozenset(
+                [
+                    AudioSegment(0, 7.464, 25.834),
+                ]
+            ),
+        ),
+    }
+        
+    received = extract_audio_sources(
+        csv_loc, url_creator=url_creator, ignore_names=["test"]
+    )
+    
+    assert set(received) == expected
+
