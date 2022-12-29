6755a588c9bc134a5757ad3850dc8856db74ecd5
Mikko Kouhia <mikko.kouhia@iki.fi>
2022-12-29 08:04:28 +0200

    Add classes for audio and segment files

--- singing_classifier/etl.py
+++ singing_classifier/etl.py
@@ -1,11 +1,12 @@
 """Download data files for the SVQTD dataset"""
 
+import subprocess
 from abc import ABC, abstractmethod
-from dataclasses import dataclass
+from collections.abc import Iterable
 from os import PathLike
 from pathlib import Path
-from collections.abc import Callable
 
+import numpy as np
 import pandas as pd
 import youtube_dl
 
@@ -65,94 +66,307 @@ class CachedFile(ABC):
         return dest_ is not None and dest_.is_file()
 
 
-@dataclass(frozen=True, eq=True)
-class AudioSegment:
-    """Time segment in audio source."""
+class YTAudio(CachedFile):
 
-    num: int
-    start_time: float
-    end_time: float
+    """YouTube audio file."""
 
-
-@dataclass(frozen=True, eq=True)
-class AudioSource:
-    """Audio source, with segment information."""
-
-    name: str
-    url: str
-    segments: frozenset[AudioSegment]
-
-    def get_youtube(self, dest_dir: PathLike, force=False) -> Path:
-        """Download video content from YouTube.
+    def __init__(
+        self,
+        tag: str,
+        segment_splits: dict[int, tuple[float, float]] = None,
+        ydl_opts=None,
+    ):
+        """Create descriptor of YouTube audio file
 
         Args:
-            url: URL to YouTube video, in format
-                'https://www.youtube.com/watch?v=...'
-            dest_dir: Destination directory location on disk.
-            force: Force download. If false, use already downloaded content.
+            tag: YouTube video tag, e.g. in an url like
+              https://www.youtube.com/watch?v=..., tag is the value of
+              the url parameter `v`.
+            segment_splits: Description of how to split file into audio
+              segments. This is employed by `self.form_audio_segments`.
+            ydl_opts: Additional options to youtube-dl.
+        """
+        super().__init__()
+        self.tag = tag
+        self.segment_splits = segment_splits or {}
+        self.ydl_opts = ydl_opts or {}
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, YTAudio):
+            return False
+
+        return (
+            self.tag == __o.tag
+            and self.segment_splits == __o.segment_splits
+            and self.ydl_opts == __o.ydl_opts
+        )
+
+    def __hash__(self) -> int:
+        return hash(
+            (self.tag, tuple(self.segment_splits.items()), tuple(self.ydl_opts.items()))
+        )
+
+    def __repr__(self) -> str:
+        return (
+            f"YTAudio({repr(self.tag)}, segment_splits={self.segment_splits}, "
+            f"ydl_opts={self.ydl_opts})"
+        )
+
+    @property
+    def _file_glob(self) -> str:
+        return f"{self.tag}.*"
+
+    @property
+    def url(self) -> str:
+        """Returns URL from youtube video ID."""
+        return f"https://www.youtube.com/watch?v={self.tag}"
+
+    def get(self, cache_dir: Path = None, force=False) -> Path:
+        """Download audio from YouTube.
+
+        Attempt to serve content from cache, if present. File
+        location is stored in `self.dest`.
+
+        Args:
+            cache_dir: Directory, where cached content is stored.
+            force: Download even if file is already found in cache.
 
         Returns:
-            Location of downloaded file on disk.
+            File location for the downloaded file.
+
+        Raises:
+            UserWarning on failed download.
         """
-        dest_dir_path = Path(dest_dir)
+        super().get(cache_dir, force)
+        if not force and self.is_cached:
+            return self.dest
+
         ydl_opts = {
             "format": "bestaudio/best",
-            "outtmpl": str(dest_dir_path.absolute()) + "/%(id)s.%(ext)s",
+            "outtmpl": str(self.cache_dir.absolute()) + "/%(id)s.%(ext)s",
+            # TODO Examine the need for running postprocessor at this stage
             # 'postprocessors': [{
             #     'key': 'FFmpegExtractAudio',
-            #     'preferredcodec': 'mp3',
-            #     'preferredquality': '192',
+            #     'preferredcodec': 'best',
             # }],
-        }
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl_return_code = ydl.download([self.url])
+        } | self.ydl_opts
+        try:
+            with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+                ydl_return_code = ydl.download([self.url])
+        except youtube_dl.DownloadError as exc:
+            raise UserWarning(f"Download failed on {self.url}, {ydl_opts=}") from exc
 
         if ydl_return_code == 0:
-            matching_files = list(dest_dir_path.glob(f"{self.name}.*"))
+            matching_files = list(self.cache_dir.glob(f"{self.tag}.*"))
             assert len(matching_files) == 1
-            # TODO set downloaded location to object attributes
-            return matching_files[0]
-        raise NotImplementedError
+            file_name = matching_files[0]
+        else:
+            raise UserWarning("Download failed")
 
+        self._dest = self.cache_dir / file_name
+        return self.dest
 
-def to_youtube_url(tag: str) -> str:
-    """Returns URL from youtube video ID."""
-    return "https://www.youtube.com/watch?v=" + tag
+    def form_segments(self, **segment_kwargs) -> list["AudioSegment"]:
+        """Returns list of AudioSegment objects."""
+        return [
+            AudioSegment(i, *split, source=self, **segment_kwargs)
+            for i, split in self.segment_splits.items()
+        ]
 
+    @classmethod
+    def from_csv(
+        cls,
+        source_csv: PathLike,
+    ) -> list["YTAudio"]:
+        """Create group of audio sources from dataframe."""
+        data = pd.read_csv(source_csv)[["name", "num", "time_start", "time_end"]]
 
-def extract_audio_sources(
-    source_csv: PathLike,
-    url_creator: Callable = to_youtube_url,
-    ignore_names: list[str] = None,
-) -> list[AudioSource]:
-    data = pd.read_csv(source_csv)[["name", "num", "time_start", "time_end"]]
-    ignore_names = ignore_names or []
+        grouped = data.groupby("name")
+        return [
+            cls._grouped_df_to_cls(group_name, grouped.get_group(group_name))
+            for group_name in grouped.groups
+        ]
 
-    grouped = data.groupby("name")
-    return [
-        _grouped_df_to_audiosource(
-            name=group_name,
-            data=grouped.get_group(group_name),
-            url_creator=url_creator,
+    @classmethod
+    def _grouped_df_to_cls(cls, name: str, data: pd.DataFrame) -> "YTAudio":
+        segments = (
+            data.sort_values(by="num", ascending=True)
+            .apply(lambda r: (r["num"], (r["time_start"], r["time_end"])), axis=1)
+            .tolist()
         )
-        for group_name in grouped.groups
-        if group_name not in ignore_names
-    ]
+        return cls(name, dict(segments))
 
 
-def _grouped_df_to_audiosource(
-    name: str, data: pd.DataFrame, url_creator: Callable
-) -> AudioSource:
-    segments = (
-        data.sort_values(by="num", ascending=True)
-        .apply(_row_to_segment, axis=1)
-        .tolist()
-    )
-    return AudioSource(name=name, url=url_creator(name), segments=frozenset(segments))
+class AudioSegment(CachedFile):
 
+    """Audio segment, from YouTube audio."""
 
-def _row_to_segment(row):
-    return AudioSegment(row["num"], row["time_start"], row["time_end"])
+    summary_columns = ["tag", "num", "path"]
+    formats = ["copy", "wav", "flac", "opus", "ogg"]
+
+    def __init__(
+        self,
+        id_: int,
+        time_start: float,
+        time_end: float,
+        /,
+        source: YTAudio = None,
+        *,
+        sample_rate: int = None,
+        format_: str = "copy",
+    ):
+        """Initialize audio segment.
+
+        Args:
+            id_ (int): _description_
+            time_start (float): _description_
+            time_end (float): _description_
+            source (YTAudio, optional): _description_. Defaults to None.
+            sample_rate: Output audio file sample rate. If None, do not
+              resample when cropping the audio. If format is 'opus',
+              sample_rate must be 48000 or None.
+            format_: Output file format. Must be one of 'copy', 'wav',
+              'flac', 'opus' or 'ogg'. If 'copy', use the same encoding
+              as in the source file.
+
+        Raises:
+            ValueError if format is not one of supported values, or
+            sample_rate is incorrect on opus format.
+        """
+        super().__init__()
+        self.id_ = id_
+        self.time_start = time_start
+        self.time_end = time_end
+        self.source = source
+
+        self.format = format_ or "copy"
+
+        self.ffmpeg_extra = ["-ac", "1"]
+        if sample_rate is not None:
+            self.ffmpeg_extra.extend(["-ar", str(sample_rate)])
+
+        if format_ not in self.formats:
+            raise ValueError(f"Unsupported format: {format_}")
+        if format_ == "opus" and sample_rate not in [48000, None]:
+            raise ValueError("For opus format, sample_rate must be 48000 or None.")
+
+    def __eq__(self, __o: object) -> bool:
+        if not isinstance(__o, AudioSegment):
+            return False
+
+        return (
+            self.id_ == __o.id_
+            and self.time_start == __o.time_start
+            and self.time_end == __o.time_end
+            and self.source == __o.source
+            and self.format == __o.format
+            and self.ffmpeg_extra == __o.ffmpeg_extra
+        )
+
+    def __hash__(self) -> int:
+        return hash(
+            (
+                self.id_,
+                self.time_start,
+                self.time_end,
+                self.source,
+                self.format,
+                tuple(self.ffmpeg_extra),
+            )
+        )
+
+    @property
+    def _file_glob(self) -> str:
+        return f"{self.source.tag}_{self.id_}.*"
+
+    def get(self, cache_dir: Path = None, force=False) -> Path:
+        """Split audio segment from source file.
+
+        Attempt to serve content from cache, if present. File
+        location is stored in `self.dest`.
+
+        Args:
+            cache_dir: Directory, where cached content is stored.
+            force: Download even if file is already found in cache.
+
+        Returns:
+            File location for the split file.
+
+        Raises:
+            UserWarning if source is not set or cached,
+              or on conversion failure.
+        """
+        super().get(cache_dir, force)
+        if not force and self.is_cached:
+            return self.dest
+
+        if self.source is None:
+            raise UserWarning("Audio source is not set.")
+        if not self.source.is_cached:
+            raise UserWarning("Audio source is not cached.")
+
+        source_path = self.source.dest
+        suffix = source_path.suffix if self.format == "copy" else self.format
+        dest = self.cache_dir / f"{self.source.tag}_{self.id_}.{suffix}"
+
+        call_args = [
+            "-y",
+            "-vn",
+            "-i",
+            str(source_path.absolute()),
+            "-ss",
+            f"{self.time_start:.3f}",
+            "-to",
+            f"{self.time_end:.3f}",
+        ]
+
+        if self.format == "ogg":
+            acodec_opts = ["-acodec", "libvorbis"]
+        elif self.format == "opus":
+            acodec_opts = ["-acodec", "libopus"]
+        elif self.format == "wav":
+            acodec_opts = []
+        else:
+            acodec_opts = ["-acodec", self.format]
+
+        command = (
+            ["ffmpeg"]
+            + call_args
+            + acodec_opts
+            + self.ffmpeg_extra
+            + [str(dest.absolute())]
+        )
+        try:
+            subprocess.run(command, check=True)
+        except subprocess.SubprocessError as err:
+            raise UserWarning(
+                f"Audio conversion error, command: '{' '.join(command)}'"
+            ) from err
+
+        self._dest = dest
+        return self.dest
+
+    @classmethod
+    def summarize(cls, segments: Iterable["AudioSegment"]) -> pd.DataFrame:
+        """Returns summary dataframe of all segments.
+
+        Columns are
+        - `tag`: tag for original audio
+        - `num`: segment ID number
+        - `path`: path to cached file location on disk.
+        """
+        items = [
+            {
+                "tag": seg.source.tag if seg.source is not None else np.nan,
+                "num": seg.id_,
+                "path": str(seg.dest) if seg.dest is not None else np.nan,
+            }
+            for seg in segments
+        ]
+        data = pd.DataFrame(items)
+        for col in ["tag", "path"]:
+            data[col] = data[col].astype("string")
+        return data
 
 
 if __name__ == "__main__":
--- tests/test_etl.py
+++ tests/test_etl.py
@@ -2,15 +2,41 @@
 
 from pathlib import Path
 
+import numpy as np
+import pandas as pd
 import pytest
+from pandas.testing import assert_frame_equal
+from scipy.io import wavfile
 
-from singing_classifier.etl import (
-    AudioSegment,
-    AudioSource,
-    CachedFile,
-    extract_audio_sources,
-    to_youtube_url,
-)
+from singing_classifier.etl import AudioSegment, CachedFile, YTAudio
+
+
+def _create_sine(
+    freq: float,
+    length: float,
+    sample_rate: int,
+    start_phase: int = 0.0,
+) -> np.ndarray[np.int16]:
+    """Returns sine wave signal in 16 bit int format.
+
+    Credit: https://docs.scipy.org/doc/scipy/reference/generated/scipy.io.wavfile.write.html
+    """
+    time = np.linspace(0.0, length, int(sample_rate * length))
+    amplitude = np.iinfo(np.int16).max
+    return (amplitude * np.sin(2 * np.pi * freq * time + start_phase)).astype(np.int16)
+
+
+@pytest.fixture(name="audio_file", scope="module")
+def fx_test_audio(tmp_path_factory) -> Path:
+    """Create test audio file on disk, return its location."""
+    sample_rate = 44100
+    data = _create_sine(freq=440.0, length=2.0, sample_rate=sample_rate)
+
+    loc = tmp_path_factory.mktemp("data") / "test_audio.wav"
+    with open(loc, "wb") as file_:
+        wavfile.write(file_, sample_rate, data)
+
+    return loc
 
 
 class CacheImpl(CachedFile):
@@ -89,65 +115,121 @@ class TestCache:
         assert received == tmp_path / "subdir" / "test_id.1"
 
 
-def test_to_youtube_url():
-    """Add tag at the end."""
-    tag = "abc"
-    expected = "https://www.youtube.com/watch?v=" + tag
-    assert to_youtube_url(tag) == expected
+class TestYTAudio:
 
+    """Test audio extraction and segment forming."""
 
-def test_extract_audio_sources(tmp_path: Path):
-    """Convert CSV file into list of audio sources."""
-    content = """name,num,time_start,time_end
-test,0,0.0,0.0
+    @pytest.mark.integration_test
+    def test_get(self, tmp_path: Path):
+        """File is downloaded from YouTube."""
+        tag = "BaW_jenozKc"
+        source = YTAudio(tag, ydl_opts={"cachedir": False})
+
+        received = source.get(cache_dir=tmp_path)
+        assert received == tmp_path / f"{tag}.m4a"
+        assert received.is_file()
+
+    @pytest.mark.parametrize(
+        "sample_rate,format_",
+        [
+            (None, None),
+            (28000, None),
+            (None, "copy"),
+            (14000, "wav"),
+        ],
+    )
+    def test_form_segments(self, sample_rate: int, format_: str):
+        """Segment objects are created."""
+        splits = {7: (0.8, 22.788), 10: (35.401, 45.023)}
+        source = YTAudio("test", splits)
+
+        kwargs = {"sample_rate": sample_rate, "format_": format_}
+        kwargs = {k: v for k, v in kwargs.items() if v is not None}
+
+        received = source.form_segments(**kwargs)
+        expected = [
+            AudioSegment(7, 0.8, 22.788, source, **kwargs),
+            AudioSegment(10, 35.401, 45.023, source, **kwargs),
+        ]
+
+        assert received == expected
+
+    def test_from_csv(self, tmp_path: Path):
+        """Convert CSV file into list of audio sources."""
+        content = """name,num,time_start,time_end
 0HVjTPqe5I0,0,8.893,17.323
 0HVjTPqe5I0,1,18.053,28.832
 0HVjTPqe5I0,2,39.12,53.974
 0QzJ86rMHzQ,0,7.464,25.834"""
-    csv_loc = tmp_path / "test.csv"
-    csv_loc.write_text(content)
+        csv_loc = tmp_path / "test.csv"
+        csv_loc.write_text(content)
 
-    def url_creator(tag):
-        return "abc" + tag
-
-    expected = {
-        AudioSource(
-            "0HVjTPqe5I0",
-            url_creator("0HVjTPqe5I0"),
-            frozenset(
-                [
-                    AudioSegment(0, 8.893, 17.323),
-                    AudioSegment(1, 18.053, 28.832),
-                    AudioSegment(2, 39.12, 53.974),
-                ]
+        expected = {
+            YTAudio(
+                "0HVjTPqe5I0",
+                {
+                    0: (8.893, 17.323),
+                    1: (18.053, 28.832),
+                    2: (39.12, 53.974),
+                },
             ),
-        ),
-        AudioSource(
-            "0QzJ86rMHzQ",
-            url_creator("0QzJ86rMHzQ"),
-            frozenset(
-                [
-                    AudioSegment(0, 7.464, 25.834),
-                ]
-            ),
-        ),
-    }
+            YTAudio("0QzJ86rMHzQ", {0: (7.464, 25.834)}),
+        }
 
-    received = extract_audio_sources(
-        csv_loc, url_creator=url_creator, ignore_names=["test"]
+        received = YTAudio.from_csv(csv_loc)
+
+        assert set(received) == expected
+
+
+class TestSegment:
+
+    """Test audio splitting."""
+
+    @pytest.mark.parametrize(
+        "sample_rate,format_",
+        [
+            (16000, "wav"),
+            (None, "copy"),
+            (44100, "flac"),
+            (None, "opus"),
+            (48000, "opus"),
+            (32000, "ogg"),
+        ],
     )
+    def test_get(
+        self, audio_file: Path, tmp_path: Path, sample_rate: str, format_: str
+    ):
+        """File is cropped and resampled from source."""
+        segment = AudioSegment(0, 0.1, 1.0, sample_rate=sample_rate, format_=format_)
+        segment.source = YTAudio(None)
+        segment.source.dest = audio_file
 
-    assert set(received) == expected
+        segment_dest = segment.get(cache_dir=tmp_path)
+        assert segment.dest.is_file()
+        assert segment.dest == segment_dest
 
+    def test_summarize(self):
+        """Summary dataframe is created."""
+        source_aa = YTAudio("aa")
+        segments = [AudioSegment(i, None, None) for i in [0, 1, 0]]
+        segments[0].source = source_aa
+        segments[1].source = source_aa
+        segments[2].source = YTAudio("ab")
 
-@pytest.mark.integration_test
-def test_get_youtube(tmp_path: Path):
-    """File from URL is stored onto disk"""
-    source = AudioSource(
-        "BaW_jenozKc", "https://www.youtube.com/watch?v=BaW_jenozKc", frozenset()
-    )
+        for seg in segments:
+            seg.dest = Path("tmp", f"{seg.source.tag}_{seg.id_}.m4a")
 
-    received_path = source.get_youtube(dest_dir=tmp_path)
+        received = AudioSegment.summarize(segments)
 
-    assert received_path.with_suffix("") == tmp_path / source.name
-    assert received_path.is_file()
+        vals = [
+            ("aa", 0, "tmp/aa_0.m4a"),
+            ("aa", 1, "tmp/aa_1.m4a"),
+            ("ab", 0, "tmp/ab_0.m4a"),
+        ]
+        data = [dict(zip(AudioSegment.summary_columns, i)) for i in vals]
+        expected = pd.DataFrame(data)
+
+        for col in ["tag", "path"]:
+            expected[col] = expected[col].astype("string")
+
+        assert_frame_equal(received, expected)
